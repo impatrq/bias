954012fb39fdc6ea01ea437e591b73dcd602fdd3
Nicolas Adell <81983008+NicolasAdell@users.noreply.github.com>
2024-06-08 19:17:03 -0300

    fix: modificar para lectura de ADC

--- codigo/Recepcion/recepcion.py
+++ codigo/Recepcion/recepcion.py
@@ -8,9 +8,10 @@ def main():
 
     try:
         while True:
-            write_data(bus, 0x10, I2C_SLAVE_ADDRESS)  # Example data to send
-            time.sleep(1)
-            read_data(bus, I2C_SLAVE_ADDRESS)
+            adc_channel = 0x00
+            write_data(bus, adc_channel, I2C_SLAVE_ADDRESS)  # Example data to send
+            time.sleep(0.1)
+            adc_value = read_adc_value(bus, I2C_SLAVE_ADDRESS)
             time.sleep(1)
     except KeyboardInterrupt:
         print("Program stopped")
@@ -23,10 +24,10 @@ def write_data(bus, data, address):
     print(f"Sent data: {data}")
 
 # Function to read data from the I2C slave
-def read_data(bus, address):
-    data = bus.read_byte(address)
-    print(f"Received data: {data}")
-    return data
+def read_adc_value(bus, address):
+    data = bus.read_i2c_block_data(address, 0, 2)
+    adc_value = (data[0] << 8) | data[1]
+    return adc_value
 
 if __name__ == "__main__":
     main()
