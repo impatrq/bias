36a675b6920fa13591bc59b23a075ce099d12ed6
Nicolas Adell <81983008+NicolasAdell@users.noreply.github.com>
2024-06-09 12:26:51 -0300

    style: add comments

--- codigo/Recepcion/recepcion.py
+++ codigo/Recepcion/recepcion.py
@@ -4,13 +4,18 @@ import time
 def main():
     I2C_SLAVE_ADDRESS = 0x04
     I2C_BUS = 1
+
+    # Set i2c bus
     bus = smbus2.SMBus(I2C_BUS)
 
     try:
         while True:
+            # Set the ADC channel to read
             adc_channel = 0x00
-            write_data(bus, adc_channel, I2C_SLAVE_ADDRESS)  # Example data to send
+            # Example of data to send
+            write_data(bus, adc_channel, I2C_SLAVE_ADDRESS)
             time.sleep(0.1)
+            # Read value from the ADC
             adc_value = read_adc_value(bus, I2C_SLAVE_ADDRESS)
             time.sleep(1)
     except KeyboardInterrupt:
@@ -26,6 +31,7 @@ def write_data(bus, data, address):
 # Function to read data from the I2C slave
 def read_adc_value(bus, address):
     data = bus.read_i2c_block_data(address, 0, 2)
+    # Sum the two bytes of the ADC
     adc_value = (data[0] << 8) | data[1]
     return adc_value
 
