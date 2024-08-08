3c6295cd880d18022f1c02e6b659060ca9f99ba0
Nicolas Adell <81983008+NicolasAdell@users.noreply.github.com>
2024-08-08 10:38:34 -0300

    chore: redundation

--- codigo/EEG/filtering.py
+++ codigo/EEG/filtering.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.signal import butter, filtfilt, firwin, lfilter, iirfilter, firwin, lfilter
+from scipy.signal import butter, filtfilt, firwin, lfilter, iirfilter
 from sklearn.decomposition import FastICA
 import preprocessing
 import matplotlib.pyplot as plt
